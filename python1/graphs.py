2fc24c5471e9c08abac72fb578ad5186eefd6b92
Akanksha10029 <112504655+Akanksha10029@users.noreply.github.com>
2023-09-23 15:18:11 +0000

    first commit

---
+++ python1/graphs.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+semesters=[1,2,3,4,5,6,7,8]
+cgpa=[7.8,8.2,8,7.6,8,7.9,7.6,8]
+plt.figure(figsize=(8, 5))
+plt.pie(cgpa,labels=semesters, autopct='%1.1f%%')
+plt.title("variation of cgpa with semesters")
+plt.legend(["cgpa"], loc="upper right")
